a899c8930b80761858c3058f2c8ea333d8264df7
tiagoboucas <tiagoboucas@hotmail.com>
2023-09-01 16:58:42 +0100

    add npm info

--- index.js
+++
@@ -1,9 +0,0 @@
-// This is js
-// Write as much code as you want here 
-const peachBlossom = {
-  bloom () {
-    console.log('Bloom!')
-  }
-}
-// Exports peachBlossom for use in other files
-module.exports = peachBlossom
